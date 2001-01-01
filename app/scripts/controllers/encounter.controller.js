accde15c1e57d403cc0dd8af64c601df6da0b871
Jason Hamm <jhamm.business@gmail.com>
2000-12-31 19:03:14 -0500

    Create encounter service

--- app/scripts/controllers/encounter.controller.js
+++ app/scripts/controllers/encounter.controller.js
@@ -1,10 +1,20 @@
 'use strict';
 
-angular.module('clinicalApp').controller('EncounterCtrl', function ($scope, encounterService) {
-  encounterService.search({
+angular.module('clinicalApp').controller('EncounterCtrl', function ($scope, providerService, encounterService) {
+  providerService.search({
     limit: 2000,
     organizationId : '0001194'
   }, function(data) {
     $scope.providers = data.providers;
+  });
+
+  encounterService.search({
+    actorType: 'PROVIDER_REQUESTING',
+    actorIds: '0001194',
+    actionStatuses: 'NOT_STARTED',
+    actionType: 'AUTO_AUTH',
+    limit: 10
+  }, function(data) {
+    $scope.encounters = data.encounters;
   });
 });
--- app/scripts/services/encounter.service.js
+++ app/scripts/services/encounter.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('clinicalApp').factory('encounterService', function ($resource) {
-  var EncounterService = $resource('http://localhost:port/v1/express-providers/:providerId', {providerId:'@id', port: ':8280'}, {
+  var EncounterService = $resource('http://localhost:port/v2/encounters/:encounterId', {encounterId:'@id', port: ':8280'}, {
     search: {
       method: 'GET',
       headers: {
