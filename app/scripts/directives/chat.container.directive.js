01bb96d2e02bb3cc781f229b918c44637d3c83b7
Jason Hamm <jhamm.business@gmail.com>
2013-10-15 15:08:44 -0400

    access the count for the chat window

--- app/scripts/directives/chat.container.directive.js
+++ app/scripts/directives/chat.container.directive.js
@@ -4,12 +4,13 @@ angular.module('clinicalApp').directive('chatContainer', function() {
   return {
     restrict: 'A',
     scope: {
-      encounter: '='
+      encounter: '=',
+      count: '='
     },
     templateUrl: 'views/chat.container.html',
 
     link: function(scope, elem, attrs) {
-
+      scope.count = 500;
     }
   };
 });
