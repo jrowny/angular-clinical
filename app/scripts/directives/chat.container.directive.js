ad818b74b545f2b537395de1ea7e90c6853e2422
Jason Hamm <jhamm.business@gmail.com>
2013-10-16 12:53:08 -0400

    Functions in chat window to try to update count

--- app/scripts/directives/chat.container.directive.js
+++ app/scripts/directives/chat.container.directive.js
@@ -7,10 +7,19 @@ angular.module('clinicalApp').directive('chatContainer', function() {
       encounter: '=',
       count: '='
     },
+
     templateUrl: 'views/chat.container.html',
 
     link: function(scope, elem, attrs) {
       scope.count = 500;
+
+      scope.toggle = function() {
+        console.log("Toggle");
+      };
+
+      scope.handleKeypress = function() {
+        console.log('Keypress');
+      };
     }
   };
 });
