62d6a07635032d80035b5d35ecbccd90b3a4939f
Jason Hamm <jhamm.business@gmail.com>
2013-10-16 12:51:26 -0400

    Directive to handle keyup events

--- app/scripts/directives/keyup.directive.js
+++ app/scripts/directives/keyup.directive.js
@@ -1,17 +1,17 @@
 'use strict';
 
-angular.module('clinicalApp').directive('onKeyupFn', function() {
-    return function(scope, elm, attrs) {
-        //Evaluate the variable that was passed
-        //In this case we're just passing a variable that points
-        //to a function we'll call each keyup
-        var keyupFn = scope.$eval(attrs.onKeyupFn);
-        elm.bind('keyup', function(evt) {
-            //$apply makes sure that angular knows
-            //we're changing something
-            scope.$apply(function() {
-                keyupFn.call(scope, evt.which);
-            });
+angular.module('clinicalApp').directive('keyup', function() {
+  return {
+    restrict: 'A',
+
+    link: function(scope, elem, attrs) {
+      console.log(scope);
+      var keyupFn = scope.$eval(attrs.onKeyupFn);
+      elem.bind('keyup', function(evt) {
+        scope.$apply(function() {
+          keyupFn.call(scope, evt.which);
         });
-    };
+      });
+    }
+  };
 });
