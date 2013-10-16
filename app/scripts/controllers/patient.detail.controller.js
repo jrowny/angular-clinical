16dcfdbc0251683f0c10db555ae718914a33a8d9
Dane Russell <drussell@c02g3129df91.local>
2013-10-16 13:30:55 -0400

    adding the form stuff

--- app/scripts/controllers/patient.detail.controller.js
+++ app/scripts/controllers/patient.detail.controller.js
@@ -19,41 +19,7 @@ angular.module('clinicalApp').controller('PatientDetailCtrl', function ($scope, 
     $scope.providers = data.providers;
   });
 
-  $scope.getRequestTypeDropDown = function() {
-    for(var i=0; i<$scope.dropDownConfigurations.length; i++) {
-      if($scope.dropDownConfigurations[i].field === 'requestType'){
-        return $scope.dropDownConfigurations[i];
-      }
-    }
-  }
-
-  $scope.getServiceTypeDropDown = function(requestTypeValue) {
-    console.log('hello world');
-    for(var i=0; i<$scope.dropDownConfigurations.length; i++) {
-      if($scope.dropDownConfigurations[i].field === 'requestType' && hasDependency(requestTypeValue, $scope.dropDownConfigurations[i].values)){
-        return $scope.dropDownConfigurations[i];
-      }
-    }
-  }
-
-  $scope.test = function() {
-    console.log("here I am");
-  }
-
-  $scope.hasDependency = function(dependency, dependencyList) {
-    for(var i=0; i<dependencyList.length; i++) {
-      if(dependencyList[i] === dependency) {
-        return true;
-      }
-      return false;
-    }
-  }
-
-  formConfiguration.search({}, function(data) {
-    $scope.formConfiguration = data;
-    $scope.dropDownConfigurations = data.formConfiguration.dropDownConfigurations;
-    $scope.requestTypeDropDown = $scope.getRequestTypeDropDown();
-  });
+ 
 
 
   //$scope.encounter = singleEncounter.selectedEncounter;
--- app/scripts/directives/new.auth.directive.js
+++ app/scripts/directives/new.auth.directive.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('clinicalApp').directive('newAuth', function() {
+angular.module('clinicalApp').directive('newAuth', ['formConfiguration', function(formConfiguration) {
 	return {
 		restrict: 'A',
 		scope: {
@@ -12,7 +12,85 @@ angular.module('clinicalApp').directive('newAuth', function() {
 		templateUrl: 'views/new.auth.html',
  
 		link: function(scope, elem, attrs) {
-			
+
+			scope.diagnosisCodes = [];
+			scope.procedureCodes = [];
+
+			scope.addDiagnosisCode = function(newCode) {
+				scope.diagnosisCodes.push(newCode);
+			}
+
+			scope.addProcedureCode = function(newCode) {
+				scope.procedureCodes.push(newCode);
+			}
+
+			scope.test = function() {
+				console.log("ya been tested");
+			}
+
+			//***********
+
+			scope.populateDropDowns = function(requestTypeValue) {
+				scope.getPlaceOfServiceDropDown(requestTypeValue);
+				scope.getServiceTypeDropDown(requestTypeValue);
+			}
+
+			scope.getPlaceOfServiceDropDown = function (requestTypeValue) {
+				for(var i=0; i<scope.dropDownConfigurations.length; i++) {
+					if(scope.dropDownConfigurations[i].field === 'servicePlace' && scope.hasDependency(requestTypeValue, scope.dropDownConfigurations[i].dependencies)){
+						scope.servicePlaceDropDown = scope.dropDownConfigurations[i];
+						return scope.dropDownConfigurations[i];
+					}
+				}
+			}
+
+			scope.getRequestTypeDropDown = function() {
+				for(var i=0; i<scope.dropDownConfigurations.length; i++) {
+					if(scope.dropDownConfigurations[i].field === 'requestType'){
+						return scope.dropDownConfigurations[i];
+					}
+				}
+			}
+
+			scope.getServiceTypeDropDown = function(requestTypeValue) {
+				for(var i=0; i<scope.dropDownConfigurations.length; i++) {
+					if(scope.dropDownConfigurations[i].field === 'serviceType' && scope.hasDependency(requestTypeValue, scope.dropDownConfigurations[i].dependencies)){
+						scope.serviceTypeDropDown = scope.dropDownConfigurations[i];
+						return scope.dropDownConfigurations[i];
+					}
+				}
+			}
+
+			scope.hasDependency = function(dependency, dependencyList) {
+				if(dependencyList.requestType === dependency) {
+						return true;
+				}
+				return false;
+			}
+
+			scope.submit = function() {
+				var newAuthInfo = {}
+
+				newAuthInfo.requestType = scope.currentRequestType;
+				newAuthInfo.serviceType = scope.currentServiceType;
+				newAuthInfo.placeOfService = scope.currentPlaceOfService;
+				scope.datepicker.date.setDate(scope.datepicker.date.getDate() + 1);
+				newAuthInfo.serviceDate = scope.datepicker.date.toDateString();
+				newAuthInfo.diagnosisCodes = scope.diagnosisCodes;
+				newAuthInfo.procedureCodes = scope.procedureCodes;
+				newAuthInfo.referToProvider = scope.currentProvider;
+				newAuthInfo.referToFacillity = scope.currentFacility;
+				scope.result = newAuthInfo;
+			}
+
+			formConfiguration.search({}, function(data) {
+				scope.formConfiguration = data;
+				scope.dropDownConfigurations = data.formConfiguration.dropDownConfigurations;
+				scope.requestTypeDropDown = scope.getRequestTypeDropDown();
+			});
+			//***********
+
+
 		}
 	};
-});
+}]);
